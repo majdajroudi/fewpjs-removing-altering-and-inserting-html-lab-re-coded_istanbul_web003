5df9a9ebce988acb7eab6c76845c5dc8f2ee1219
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-31 22:27:43 +0100

    commit tree fc539686bec3785d4c19f0ccd88b504688284ce2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainElement = document.querySelector("#main")
+console.log(mainElement)
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.removeChild(mainElement)
+console.log(document.body)
+document.addEventListener("load", function(){
+    document.body.removeChild(mainElement)
+    
+    document.body.appendChild(newHeader)
+})
